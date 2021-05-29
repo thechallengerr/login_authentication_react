3dcd1d08de3fbc381a6a94376dbec79a45081c00
thechallengerr <74823368+thechallengerr@users.noreply.github.com>
2021-05-29 10:41:34 +0700

    2nd changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './img/logo.svg';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Hoang',
+  lastName: 'Nguyen'
+};
+
+
+const element = (
+  <div>
+    <h1>
+    Hello, {formatName(user)}! Welcome to React
+    </h1>
+    <img srcSet={logo} className='user'></img>
+  </div>
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
